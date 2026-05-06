e571050e57a62a09b091e77388762fd6d8fa9675
dabboloosefun <146081681+dabboloosefun@users.noreply.github.com>
2026-05-06 10:19:50 +0200

    Push

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,7 +1,32 @@
 ﻿class Program {
     static void Main(string[] args){
         // Dependency injection: wiring up our components
+        Format.WriteTitle("SELECT YOUR COLLECTION TYPE");
+
+        Format.WriteList([
+            "Array", "LinkedList", "BinaryTree", "HashMap", "Manage Task Dependency"
+        ], (Console.WindowHeight - 4) / 2);
+
+        int option = 0;
+        while (option <= 0)
+        {
+            string input = Format.PromptReadKey("Select an option:");
+
+            try
+            {
+                int parsed = int.Parse(input);
+                if (parsed > 0 && parsed < 5)
+                {
+                    option = parsed;
+                }
+            }
+            catch (FormatException)
+            {
+                continue;
+            }
+        }
         
+
         string basePath = AppDomain.CurrentDomain.BaseDirectory;
         string dataPath = Path.Combine(basePath, "Data");
 
@@ -9,8 +34,10 @@
 
         string TfilePath = Path.Combine(dataPath, "tasks.json");
         string MfilePath = Path.Combine(dataPath, "members.json");
-        IRepository<TaskItem> taskRepository = new JsonRepository<TaskItem>(TfilePath);
-        IRepository<Member> memberRepository = new JsonRepository<Member>(MfilePath);
+
+        IRepository<TaskItem> taskRepository = new JsonRepository<TaskItem>(TfilePath, option);
+        IRepository<Member> memberRepository = new JsonRepository<Member>(MfilePath, option);
+
         ITaskService taskService = new TaskService(taskRepository);
         IMemberService memberService = new MemberService(memberRepository);
         ITaskView view = new ConsoleTaskView(taskService, memberService);
